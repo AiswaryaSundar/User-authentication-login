4717e1134efd7d48edba6df240d44a385159c88e
Parilakshya <83696928+ParilakshyaPuri@users.noreply.github.com>
2023-02-04 23:33:13 +0530

    updated index

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('register.html')
+
+@app.route("/register", methods=['post'])
+def register():
+    username = request.form["form3Example3"]
+    print (username)
