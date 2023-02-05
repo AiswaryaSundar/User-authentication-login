8bb03af4b1f97ad8397d10b1da082dc56e1fcd74
Parilakshya <83696928+ParilakshyaPuri@users.noreply.github.com>
2023-02-05 10:08:15 +0530

    registration page updated.

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+import psycopg2
 
 app = Flask(__name__)
 
@@ -11,3 +11,9 @@ def hello_world():
 def register():
     username = request.form["form3Example3"]
     print (username)
+
+def insert_values(self, ques):
+        cursor= self.conn.cursor()
+        insert_query= "INSERT into employee(name, state) VALUES (%s, %s, %s, %d)"
+        cursor.execute(insert_query, data) 
+        self.conn.close()    
